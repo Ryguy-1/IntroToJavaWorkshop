b57693ec49cef18c653d03ba0ee43af7703b448f
TJHL <league@WTS14.attlocal.net>
2016-07-14 11:58:05 -0700

    Hello12

--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -4,12 +4,16 @@ import javax.swing.JOptionPane;
 
 public class WorldDomination {
 	public static void main(String[] args) {
-		// 1. Ask the user if they know how to write code.
 
-		// 2. If they say "yes", tell them they will rule the world.
+String question1=JOptionPane.showInputDialog("Do you knoqw how to write code?");
 
-		// 3. Otherwise, wish them good luck washing dishes.
+if (question1.equalsIgnoreCase("yes")) {
+JOptionPane.showMessageDialog(null,"You will rule the world!");
+}else{
+JOptionPane.showMessageDialog(null,"Good luck washing dishes.");	
+}
+}
+
 
 	}
-}
 
